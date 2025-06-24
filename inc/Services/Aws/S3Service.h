6c895bbb363eb5bf5ee7da08a423b3232cbefecb
Ariel Kriheli <arieldk6@gmail.com>
2025-06-24 22:59:50 +0300

    more work but aws package errors

--- inc/Services/Aws/S3Service.h
+++ inc/Services/Aws/S3Service.h
@@ -2,11 +2,13 @@
 #define S3_SERVICE_H
 
 #include "AwsClient.h"
+#include "Services/Aws/RDSService.h"
 
 #include <aws/s3/S3Client.h>
 #include <aws/s3/S3ClientConfiguration.h>
 #include <aws/s3/model/BucketLocationConstraint.h>
 #include <aws/s3/model/CreateBucketRequest.h>
+#include <variant>
 
 class S3Service final {
 public:
@@ -22,6 +24,12 @@ private:
   Aws::S3::S3Client m_S3Client;
   std::unique_ptr<AwsClient> m_AwsClient;
   Aws::S3::S3ClientConfiguration m_S3ClientConfig{m_AwsClient->GetAwsClient()};
+};
+
+class Services {
+public:
+private:
+  std::vector<std::variant<S3Service, RDSService>> m_Services;
 };
 
 #endif
